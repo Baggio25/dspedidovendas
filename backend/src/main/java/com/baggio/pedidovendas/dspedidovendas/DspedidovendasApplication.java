3a75e7ab218dcbebc41f3f129a04bf758a6e87a1
rodrigo.baggio.si@gmail.com <Baggio25>
2022-05-20 13:44:53 -0300

    Projeto criado

---
+++ backend/src/main/java/com/baggio/pedidovendas/dspedidovendas/DspedidovendasApplication.java
@@ -0,0 +1,13 @@
+package com.baggio.pedidovendas.dspedidovendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DspedidovendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DspedidovendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/baggio/pedidovendas/dspedidovendas/DspedidovendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baggio.pedidovendas.dspedidovendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspedidovendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
