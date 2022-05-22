0f909686edfb559cdd46d91fbd20ab479b1bf659
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2022-05-22 19:58:40 -0300

    Criando o operacao de instaciacao

--- backend/src/main/java/com/baggio/pedidovendas/dspedidovendas/DspedidovendasApplication.java
+++ backend/src/main/java/com/baggio/pedidovendas/dspedidovendas/DspedidovendasApplication.java
@@ -1,13 +1,32 @@
 package com.baggio.pedidovendas.dspedidovendas;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.baggio.pedidovendas.dspedidovendas.domain.Categoria;
+import com.baggio.pedidovendas.dspedidovendas.repository.CategoriaRepository;
+
 @SpringBootApplication
-public class DspedidovendasApplication {
+public class DspedidovendasApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DspedidovendasApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
